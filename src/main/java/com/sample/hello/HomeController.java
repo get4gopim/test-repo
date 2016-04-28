a10d544950d1e6ab80a83002c3511be19f5c71af
get4gopi <get4gopi@GET4GOPIM>
2016-04-28 20:34:01 +0530

    login controller changed

--- src/main/java/com/sample/hello/HomeController.java
+++ src/main/java/com/sample/hello/HomeController.java
@@ -36,4 +36,18 @@ public class HomeController {
 		return "login";
 	}
 	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Locale locale, Model model) {
+		logger.info("Welcome home! the client locale is = "+ locale.toString());
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "login";
+	}
+	
 }
